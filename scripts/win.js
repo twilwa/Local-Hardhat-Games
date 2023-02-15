8cdc316955e42328d1f0d65adf4510b2c87d8afd
twilwa <twilwa@users.noreply.github.com>
2023-02-14 18:32:48 -0800

    all solutions found game5 included

--- scripts/win.js
+++ scripts/win.js
@@ -1,23 +1,28 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x9A676e781A523b5d0C0e43731313A708CB607508";
+const contractName = "Game5";
 
 async function main() {
-    // attach to the game
-    const game = await hre.ethers.getContractAt(contractName, gameAddr);
+  // attach to the game
+  const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
-    // do whatever you need to do to win the game here:
-    const tx = await game.win();
+  // do whatever you need to do to win the game here:
+  const tx1 = await game.giveMeAllowance(10000);
+  await tx1.wait();
+  const tx2 = await game.mint(10000);
+  await tx2.wait();
 
-    // did you win? Check the transaction receipt!
-    // if you did, it will be in both the logs and events array
-    const receipt = await tx.wait();
-    console.log(receipt);
+  const winnerTx = await game.win();
+
+  // did you win? Check the transaction receipt!
+  // if you did, it will be in both the logs and events array
+  const receipt = await winnerTx.wait();
+  console.log(receipt);
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
